1392b170a1bc1a374f8dde28f8e4d5cc2b312157
kulikovjob <kulikovvadimjob@gmail.com>
2024-02-21 16:24:15 +0200

    отформатированы названия маршрутов

--- src/routers/mediaRouter.ts
+++ src/routers/mediaRouter.ts
@@ -1,8 +1,21 @@
 import { Router } from 'express';
-import { getAllVideos } from '../controllers/videoController';
+import { getAllVideos, getVideoById } from '../controllers/videoController';
+import { getAllAudios, getAudioById } from '../controllers/audioController';
+import { getAllImages, getImageById } from '../controllers/imageController';
+import { getAllDocuments, getDocumentById } from '../controllers/documentController';
 
 const router = Router();
 
 router.route('/video/').get(getAllVideos);
+router.route('/video/:fileId').get(getVideoById);
+
+router.route('/audio/').get(getAllAudios);
+router.route('/audio/:fileId').get(getAudioById);
+
+router.route('/image/').get(getAllImages);
+router.route('/image/:fileId').get(getImageById);
+
+router.route('/document/').get(getAllDocuments);
+router.route('/document/:fileId').get(getDocumentById);
 
 export default router;
